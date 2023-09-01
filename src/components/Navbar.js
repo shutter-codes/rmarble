004857e243e971bd651a2a45d9e3c552fc79577a
Naitik Sharma <126913992+shutter-codes@users.noreply.github.com>
2023-09-01 23:01:39 +0530

    changes done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from "react";
+import './navbar.css'
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-yellow-300 mb-3">
+     <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-yellow-300 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-center">
-          <div className="w-full relative flex justify-center ml-4 lg:w-auto lg:static lg:block lg:justify-start">
+          <div className="nav-head w-full relative flex justify-center ml-4 lg:w-auto lg:static lg:block lg:justify-start ">
             <a
-              className="text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap   text-red-600"
+              className=" text-2xl font-bold leading-relaxed  mr-4 py-2 whitespace-nowrap   text-red-600"
               href="#"
             >
               Rajasthan Stone Frames <br></br> 
             </a>
-            <p className="justify-center items-center flex mr-3 text-red-600 font-semibold">Factory Outlet of Siddhivinayak Stones</p>
+            <p className="justify-center items-center flex mr-3 text-red-600 font-semibold ">Factory Outlet of Siddhivinayak Stones</p>
           </div>
         </div>
       </nav>
